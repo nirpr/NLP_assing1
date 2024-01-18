1e04022aa5567cff59d4af528e17845ec93507f3
sk-main <88608892+sk-main@users.noreply.github.com>
2024-01-18 11:08:05 +0200

    from last tuesday3

--- main.py
+++ main.py
@@ -1,15 +1,62 @@
 import json
+import random
+from collections import defaultdict
+
+import nltk
+from nltk import word_tokenize, ngrams
+
+
+# def create_trigram(text):
+#     trigram = {}
+#     words = text.split()
+#
+#     for i in range(len(words) - 2):
+#         key = (words[i], words[i + 1])
+#         value = words[i + 2]
+#
+#         if key in trigram:
+#             trigram[key].append(value)
+#         else:
+#             trigram[key] = [value]
+#
+#     return trigram
+
+def fill_missing_words(text, trigram_model):
+    list = []
+    words = word_tokenize(text)
+    for i in range(len(words) - 2):
+        if words[i] == "__________" and words[i + 2] == "__________":
+            key = (words[i - 1], words[i])
+            if key in trigram_model:
+                words[i] = random.choice(trigram_model[key])
+    return " ".join(words)
 
 
 def solve_cloze(input, candidates, lexicon, corpus):
     # todo: implement this function
     print(f'starting to solve the cloze {input} with {candidates} using {lexicon} and {corpus}')
 
-    return list()  # return your solution
+    with open(corpus, 'r', encoding='utf-8') as f1:
+        wiki = f1.read()
+    with open(input, 'r', encoding='utf-8') as f2:
+        text = f2.read()
+
+    # trigram = create_trigram(text)
+
+    tokens = word_tokenize(wiki) #need to seperate wiki's lines
+    trigrams = list(ngrams(tokens, 3))
+    trigram_model = defaultdict(list)
+
+    for a_trigram in trigrams:
+        trigram_model[(a_trigram[0], a_trigram[1])].append(a_trigram[2])
+
+    result_list = fill_missing_words(text, trigram_model)
+
+    return result_list  # return your solution
 
 
 if __name__ == '__main__':
-    with open('config.json', 'r') as json_file:
+    with open('config.json', 'r', encoding="utf8") as json_file:
         config = json.load(json_file)
 
     solution = solve_cloze(config['input_filename'],
--- plot_heaps_zipf_laws.py
+++ plot_heaps_zipf_laws.py
@@ -1,68 +1,121 @@
 import json
-import random
+import pickle
+import os.path
 from collections import defaultdict
+from matplotlib import pyplot as plt
+from math import log
+import seaborn as sn
+sn.set()
 
-import nltk
-from nltk import word_tokenize, ngrams
+
+def read_data(filename):
+    word2freq = defaultdict(int)
+
+    with open(filename, 'r', encoding='utf8') as fin:
+        print('reading the text file...')
+        for i, line in enumerate(fin.readlines()):
+            for word in line.split():
+                word2freq[word] += 1
+            if i % 100000 == 0:
+                print(i)
+
+    total_words = sum(word2freq.values())
+    word2nfreq = {w: word2freq[w]/total_words for w in word2freq}
+
+    return word2nfreq
 
 
-# def create_trigram(text):
-#     trigram = {}
-#     words = text.split()
+def plot_zipf_law(word2nfreq):
+    y = sorted(word2nfreq.values(), reverse=True)
+    x = list(range(1, len(y)+1))
+
+    product = [a * b for a, b in zip(x, y)]
+    print(product[:1000])  # todo: print and note the roughly constant value
+
+    y = [log(e, 2) for e in y]
+    x = [log(e, 2) for e in x]
+
+    plt.plot(x, y)
+    plt.xlabel('log(rank)')
+    plt.ylabel('log(frequency)')
+    plt.title("Zipf's law")
+    plt.show()
+
+# def read_data_for_heap(filename):
+#     vocabulary = defaultdict(int)
+#     num_of_tokens = 0
 #
-#     for i in range(len(words) - 2):
-#         key = (words[i], words[i + 1])
-#         value = words[i + 2]
+#     with open(filename, 'r', encoding='utf8') as fin:
+#         print('reading the text file...')
+#         for i, line in enumerate(fin.readlines()):
+#             for word in line.split():
+#                 vocabulary[word] += 1
+#                 num_of_tokens += 1
+#             if i % 100000 == 0:
+#                 print(i)
+#     data = (vocabulary, num_of_tokens)
+#     print("finished")
+#     return data
 #
-#         if key in trigram:
-#             trigram[key].append(value)
-#         else:
-#             trigram[key] = [value]
 #
-#     return trigram
+# def plot_heaps_law(data):
+#     vocabulary_size = len(data[0])
+#     num_of_tokens = data[1]
+#     step = max(1, vocabulary_size // 1000)
+#     y = list(range(1, vocabulary_size + 1, step))
+#     x = list(range(1, num_of_tokens + 1, step))
+#
+#     plt.plot(x, y)
+#     plt.xlabel('N')
+#     plt.ylabel('V')
+#     plt.title("heaps's law")
+#     plt.show()
 
 
-def fill_missing_words(text, trigram_model):
-    list = []
-    words = word_tokenize(text)
-    for i in range(len(words) - 2):
-        if words[i] == "__________" and words[i + 2] == "__________":
-            key = (words[i - 1], words[i])
-            if key in trigram_model:
-                words[i] = random.choice(trigram_model[key])
-    return " ".join(words)
+def read_data_for_heap(filename):
+    types = set()
+    tokens_processed = []
+    vocabulary_sizes = []
+
+    with open(filename, 'r', encoding='utf8') as fin:
+        print('reading the text file...')
+        for i, line in enumerate(fin.readlines()):
+            for word in line.split():
+                types.add(word)
+                tokens_processed.append(i + 1)
+                vocabulary_sizes.append(len(types))
+            if i % 100000 == 0:
+                print(i)
+
+    step = 10000
+    vocabulary_sizes = vocabulary_sizes[::step]
+    tokens_processed = tokens_processed[::step]
+    data = (tokens_processed, vocabulary_sizes)
+    return data
 
 
-def solve_cloze(input, candidates, lexicon, corpus):
-    # todo: implement this function
-    print(f'starting to solve the cloze {input} with {candidates} using {lexicon} and {corpus}')
+def plot_heaps_law(data):
+    x = data[0]
+    y = data[1]
 
-    with open(corpus, 'r', encoding='utf-8') as f1:
-        wiki = f1.read()
-    with open(input, 'r', encoding='utf-8') as f2:
-        text = f2.read()
-
-    # trigram = create_trigram(text)
-
-    tokens = word_tokenize(wiki) #need to seperate wiki's lines
-    trigrams = list(ngrams(tokens, 3))
-    trigram_model = defaultdict(list)
-
-    for a_trigram in trigrams:
-        trigram_model[(a_trigram[0], a_trigram[1])].append(a_trigram[2])
-
-    result_list = fill_missing_words(text, trigram_model)
-
-    return result_list  # return your solution
+    print('2')
+    # step = 10000
+    # y = vocabulary_sizes[::step]
+    # x = tokens_processed[::step]
+    print('3')
+    plt.plot(x, y)
+    plt.xlabel('Number of Tokens Processed')
+    plt.ylabel('Vocabulary Size (Number of Types)')
+    plt.title("Heap's Law")
+    plt.show()
 
 
 if __name__ == '__main__':
-    with open('config.json', 'r', encoding="utf8") as json_file:
+    with open('config.json', 'r', encoding='utf8') as json_file:
         config = json.load(json_file)
 
-    solution = solve_cloze(config['input_filename'],
-                           config['candidates_filename'],
-                           config['lexicon_filename'],
-                           config['corpus'])
-
-    print('cloze solution:', solution)
+    if not os.path.isfile('heap2.pkl'):
+        data = read_data_for_heap(config['corpus'])
+        pickle.dump(data, open('heap2.pkl', 'wb'))
+    print('1')
+    plot_heaps_law(pickle.load(open('heap2.pkl', 'rb')))
