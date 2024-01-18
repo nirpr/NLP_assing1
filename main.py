7fefff73e0fb764cbeacbc4bd12119fee53ff25b
nirpr <nirpr@mta.ac.il>
2024-01-18 13:37:05 +0200

    created the data structures

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import random
 from collections import defaultdict
 
 import nltk
-from nltk import word_tokenize, ngrams
-
+nltk.download('punkt')
 
 # def create_trigram(text):
 #     trigram = {}
@@ -23,7 +22,7 @@ from nltk import word_tokenize, ngrams
 
 def fill_missing_words(text, trigram_model):
     list = []
-    words = word_tokenize(text)
+    words = nltk.word_tokenize(text)
     for i in range(len(words) - 2):
         if words[i] == "__________" and words[i + 2] == "__________":
             key = (words[i - 1], words[i])
@@ -32,25 +31,44 @@ def fill_missing_words(text, trigram_model):
     return " ".join(words)
 
 
+def update_dicts(tokens, prev_word, lex_dict, bigram_dicts):
+    for word in tokens:
+        word = word.lower()
+        lex_dict[word] = lex_dict.get(word, 0) + 1
+        if prev_word != '':
+            if bigram_dicts[prev_word] not in bigram_dicts.keys():
+                bigram_dicts[prev_word] = {word: 1}
+            else:
+                bigram_dicts[prev_word][word] = bigram_dicts[prev_word].get(word, 0) + 1
+        prev_word = word
+
+    return prev_word
+
+
+def initialize_dicts(lexicon, corpus):
+    lexicon_dict = {}
+    bigram_dicts = {}
+
+    with open(lexicon, 'r', encoding='utf8') as f1:
+        for word in f1.readlines():  # because every line is a word in lexicon
+            lexicon_dict[word.rstrip('\n')] = 0
+
+    with open(corpus, 'r', encoding='utf-8') as f2:
+        prev_word = ''
+        for i, line in enumerate(f2.readlines()):
+            tokens = nltk.word_tokenize(line)
+            prev_word = update_dicts(tokens, prev_word, lexicon_dict, bigram_dicts)
+
+            if i % 100000 == 0:
+                print(i)
+    return lexicon_dict, bigram_dicts
+
+
 def solve_cloze(input, candidates, lexicon, corpus):
     # todo: implement this function
     print(f'starting to solve the cloze {input} with {candidates} using {lexicon} and {corpus}')
+    lex_dict, bigram_dicts = initialize_dicts(lexicon, corpus)
 
-    with open(corpus, 'r', encoding='utf-8') as f1:
-        wiki = f1.read()
-    with open(input, 'r', encoding='utf-8') as f2:
-        text = f2.read()
-
-    # trigram = create_trigram(text)
-
-    tokens = word_tokenize(wiki) #need to seperate wiki's lines
-    trigrams = list(ngrams(tokens, 3))
-    trigram_model = defaultdict(list)
-
-    for a_trigram in trigrams:
-        trigram_model[(a_trigram[0], a_trigram[1])].append(a_trigram[2])
-
-    result_list = fill_missing_words(text, trigram_model)
 
     return result_list  # return your solution
 
